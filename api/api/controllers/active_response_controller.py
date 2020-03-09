2b9d9ddf0a71d177daa1288dd7c757448d309875
Daniel Ruiz <daniel.ruiz@wazuh.com>
2020-03-09 17:42:32 +0100

    Adapt active response controller to aiohttp

--- api/api/controllers/active_response_controller.py
+++ api/api/controllers/active_response_controller.py
@@ -2,23 +2,23 @@
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 
-import asyncio
 import logging
 
 import connexion
+from aiohttp import web
 
 import wazuh.active_response as active_response
 from api.authentication import get_permissions
+from api.encoder import dumps
 from api.models.active_response_model import ActiveResponse
 from api.util import remove_nones_to_dict, exception_handler, raise_if_exc
 from wazuh.core.cluster.dapi.dapi import DistributedAPI
 
-loop = asyncio.get_event_loop()
 logger = logging.getLogger('wazuh')
 
 
 @exception_handler
-def run_command(list_agents='*', pretty=False, wait_for_complete=False):
+async def run_command(list_agents='*', pretty=False, wait_for_complete=False):
     """Runs an Active Response command on a specified agent
 
     :param list_agents: List of Agents IDs. All possible values since 000 onwards
@@ -40,7 +40,6 @@ def run_command(list_agents='*', pretty=False, wait_for_complete=False):
                           broadcasting=list_agents == '*',
                           rbac_permissions=get_permissions(connexion.request.headers['Authorization'])
                           )
-    data = raise_if_exc(loop.run_until_complete(dapi.distribute_function()))
+    data = raise_if_exc(await dapi.distribute_function())
 
-    return data, 200
-
+    return web.json_response(data, status=200, dumps=dumps)
